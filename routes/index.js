b8e3bf165708be17103ad05308c7e4e6d7f004d2
System Administrator <poole.james@gmail.com>
2015-02-27 18:10:33 -0500

    added tesnet config and nav bar difference

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var bitcore = require('bitcore');
+var Env = require('../config/env.js');
+
+var isTestNet = Env.NETWORK == bitcore.Networks.testnet;
+
+var pageData = { navColor : Env.NAV_BAR_COLOR , showTestNetWarning : isTestNet};
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', pageData);
 });
 
 
 router.get('/examples/simple', function(req, res) {
-  res.render('examples/simple', { title: 'Express' });
+  res.render('examples/simple', pageData);
 });
 
 router.get('/code', function(req, res) {
-  res.render('code', { title: 'Express' });
+  res.render('code', pageData);
 });
 
 router.get('/api', function(req, res) {
-  res.render('api', { title: 'Express' });
+  res.render('api', pageData);
 });
 
 router.get('/about', function(req, res) {
-  res.render('about', { title: 'Express' });
+  res.render('about', pageData);
 });
 
 module.exports = router;
